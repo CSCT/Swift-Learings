03f8325c398b32d89e11914aa53543db75e7b698
Colby Thompson <colbythompson123@gmail.com>
2024-07-27 22:34:30 -0400

    Started Project

---
+++ networking/CitySights App/CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Graceson Thompson on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ networking/CitySights App/CitySights App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Graceson Thompson on 7/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Friend!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
