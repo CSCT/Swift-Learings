414d3f4ce1faa85d0a4749a4cfbba89b898ef660
Colby Thompson <colbythompson123@gmail.com>
2024-08-25 14:34:07 -0400

    - build list ui in content view - dipslayed reuturned business

--- networking/CitySights App/CitySights App/ContentView.swift
+++ networking/CitySights App/CitySights App/ContentView.swift
@@ -9,24 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
+    @State var businesses = [Business]()
     @State var query: String = ""
     var service = DataService()
     
     var body: some View {
-        HStack {
-            
-            TextField("What are you looking for?", text: $query)
-                .textFieldStyle(.roundedBorder)
-            Button(action: {
-                //TODO: Implement Query
-            }, label: {
-                Text("Go")
-            })
+        VStack{
+            HStack {
+                
+                TextField("What are you looking for?", text: $query)
+                    .textFieldStyle(.roundedBorder)
+                Button(action: {
+                    //TODO: Implement Query
+                }, label: {
+                    Text("Go")
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                })
+            }
+            List(businesses){ business in
+                Text(business.name ?? "Empty")
+            }
         }
         .padding()
         .task {
-            let businesses = await service.businessSearch()
+            businesses = await service.businessSearch()
         }
     }
 }
