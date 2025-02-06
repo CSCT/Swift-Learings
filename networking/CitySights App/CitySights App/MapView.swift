ccb42307bad1fe491c5eac52a68e3c10bb859370
Colby Thompson <colbythompson123@gmail.com>
2025-02-05 19:49:41 -0500

    - added map markers and detected selection - changed api key

--- networking/CitySights App/CitySights App/MapView.swift
+++ networking/CitySights App/CitySights App/MapView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    @Environment(BusinessModel.self) var model
+    @State var selectedBusinessId: String?
+        
     var body: some View {
-        Map()
+        Map(selection: $selectedBusinessId) {
+            ForEach(model.businesses, id: \.id) { business in
+                Marker(business.name ?? "Restaurant", coordinate: CLLocationCoordinate2D(latitude: business.coordinates?.latitude ?? 0, longitude: business.coordinates?.longitude ?? 0))
+                    .tag(business.id ?? "None")
+            }
+        }
+        .onChange(of: selectedBusinessId) { oldValue, newValue in
+            //Find the Businness which matches this id
+            let business = model.businesses.first { business in
+                business.id == selectedBusinessId
+            }
+            
+            if let business = business {
+                model.selectedBusiness = business
+            }
+        }
     }
 }
 
